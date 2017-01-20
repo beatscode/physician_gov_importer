6139d74607057339bd1fd597204a4fd4ad8d4ccd
Alexander Casanova <alexcasanova2008@gmail.com>
2017-01-20 06:54:13 -0500

    proper gorm support for transactional inserts

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/csv"
 	//"encoding/json"
 	"fmt"
-	"io"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "io"
 	"os"
 	"reflect"
 	"regexp"
@@ -47,7 +49,7 @@ func getSpecialtyIDFromPhysicianSpecialty(physicianSpecialty string) string {
 var physician Physician
 var physicians []Physician
 var specialtyArray []Specialty
-var readLimit = 30
+var readLimit = 3000000
 
 func main() {
 	physicianCSV := "/Users/alex/Downloads/Physician_Compare_National_Downloadable_File.csv"
@@ -55,6 +57,13 @@ func main() {
 	specialtiesCSV := "specialties.csv"
 	physicianSpecialties := readCSV(specialtyCombinedCSV)
 	specialties := readCSV(specialtiesCSV)
+
+	db, err := gorm.Open("mysql", "db_leoprod:4DMXexDaw8s@/data_gov?charset=utf8&parseTime=True&loc=Local")
+	defer db.Close()
+
+	db.AutoMigrate(&Physician{})
+	db.Model(&Physician{}).AddIndex("idx_last_name_state", "last_name", "state")
+	//fmt.Println(db.HasTable(&Physician{}))
 
 	//Build Mapped Specialty List
 	for k, v := range specialties {
@@ -75,51 +84,56 @@ func main() {
 	checkErr(err)
 	defer filehandle.Close()
 
-	//How does one know to use *file here
 	reader := csv.NewReader(filehandle)
-
 	_, err = reader.Read()
 	checkErr(err)
 
+	var tx *gorm.DB
+	tx = db.Begin()
+	tx.Exec("truncate physicians")
 	for i := 0; i < readLimit; i++ {
-		err := Unmarshal(reader, &physician)
-		if err == io.EOF {
-			break
-		}
-		checkErr(err)
-		//loop through to find strings
-		//replace string if
+		massageAndSavePhysician(reader, tx)
+	}
+	tx.Commit()
+	//	fmt.Println(len(physicians), "physicians")
+}
 
-		CapitalizeTitle(&physician.FirstName)
-		CapitalizeTitle(&physician.LastName)
-		if physician.MedicalSchoolName == "OTHER" {
-			physician.MedicalSchoolName = ""
-		} else {
-			CapitalizeTitle(&physician.MedicalSchoolName)
-		}
+func massageAndSavePhysician(reader *csv.Reader, tx *gorm.DB) {
+	//Reads Next Record and unmarshals into physician type
+	err := Unmarshal(reader, &physician)
+	checkErr(err)
 
-		CapitalizeTitle(&physician.MiddleName)
-		CapitalizeTitle(&physician.OrganizationLegalName)
-		CapitalizeTitle(&physician.Line1StreetAddress)
-		CapitalizeTitle(&physician.Line2StreetAddress)
-		CapitalizeTitle(&physician.City)
-		physician.specialtyID = getSpecialtyIDFromPhysicianSpecialty(physician.PrimarySpecialty)
-		physicians = append(physicians, physician)
+	//loop through to find strings
+	//replace string if
+	CapitalizeTitle(&physician.FirstName)
+	CapitalizeTitle(&physician.LastName)
+	if physician.MedicalSchoolName == "OTHER" {
+		physician.MedicalSchoolName = ""
+	} else {
+		CapitalizeTitle(&physician.MedicalSchoolName)
 	}
 
-	fmt.Println(len(physicians), "physicians")
+	CapitalizeTitle(&physician.MiddleName)
+	CapitalizeTitle(&physician.OrganizationLegalName)
+	CapitalizeTitle(&physician.Line1StreetAddress)
+	CapitalizeTitle(&physician.Line2StreetAddress)
+	CapitalizeTitle(&physician.City)
+	physician.specialtyID = getSpecialtyIDFromPhysicianSpecialty(physician.PrimarySpecialty)
+	tx.Create(&physician)
+	//physicians = append(physicians, physician)
 }
 
 //CapitalizeTitle set the proper title style
 func CapitalizeTitle(title *string) {
-
+	if len(*title) == 0 {
+		return
+	}
 	*title = strings.Title(strings.ToLower(*title))
 
 	prepositions := []string{"TO", "OR", "OF", "FOR", "AT", "AND", "IN", "BY", "THE"}
 	var re *regexp.Regexp
 	for _, preposition := range prepositions {
 		matchStr := fmt.Sprintf(`\b%s\b`, strings.Title(strings.ToLower(preposition)))
-		//	fmt.Println(matchStr)
 		re = regexp.MustCompile(matchStr)
 		*title = re.ReplaceAllString(*title, strings.ToLower(preposition))
 	}
@@ -141,10 +155,13 @@ func Unmarshal(reader *csv.Reader, v interface{}) error {
 		return err
 	}
 	s := reflect.ValueOf(v).Elem()
-	if s.NumField() != len(record) {
+	/*if s.NumField() != len(record) {
 		return &FieldMismatch{s.NumField(), len(record)}
-	}
+	}*/
 	for i := 0; i < s.NumField(); i++ {
+		if i >= len(record) {
+			break
+		}
 		f := s.Field(i)
 		switch f.Type().String() {
 		case "string":
@@ -163,49 +180,49 @@ func Unmarshal(reader *csv.Reader, v interface{}) error {
 }
 
 type Physician struct {
-	NPI                                                        string
-	PACID                                                      string
-	ProfessionalEnrollmentID                                   string
-	LastName                                                   string
-	FirstName                                                  string
-	MiddleName                                                 string
-	Suffix                                                     string
-	Gender                                                     string
-	Credential                                                 string
-	MedicalSchoolName                                          string
-	GraduationYear                                             string
-	PrimarySpecialty                                           string
-	SecondarySpecialty1                                        string
-	SecondarySpecialty2                                        string
-	SecondarySpecialty3                                        string
-	SecondarySpecialty4                                        string
-	AllSecondarySpecialties                                    string
-	OrganizationLegalName                                      string
-	GroupPracticePACID                                         string
-	NumberOfGroupPracticeMembers                               string
-	Line1StreetAddress                                         string
-	Line2StreetAddress                                         string
-	MarkerOfAddressLine2Suppression                            string
-	City                                                       string
-	State                                                      string
-	ZipCode                                                    string
-	PhoneNumber                                                string
-	HospitalAffiliationCCN1                                    string
-	HospitalAffiliationLBN1                                    string
-	HospitalAffiliationCCN2                                    string
-	HospitalAffiliationLBN2                                    string
-	HospitalAffiliationCCN3                                    string
-	HospitalAffiliationLBN3                                    string
-	HospitalAffiliationCCN4                                    string
-	HospitalAffiliationLBN4                                    string
-	HospitalAffiliationCCN5                                    string
-	HospitalAffiliationLBN5                                    string
-	ProfessionalAcceptsMedicareAssignment                      string
-	ReportedQualityMeasures                                    string
-	UsedElectronicHealthRecords                                string
-	ParticipatedInTheMedicareMaintenanceOfCertificationProgram string
-	CommittedToHeartHealthThroughTheMillionHeartsInitiative    string
-	specialtyID                                                string
+	NPI                                   string
+	PACID                                 string `gorm:"column:pacid"`
+	ProfessionalEnrollmentID              string
+	LastName                              string
+	FirstName                             string
+	MiddleName                            string
+	Suffix                                string
+	Gender                                string
+	Credential                            string
+	MedicalSchoolName                     string
+	GraduationYear                        string
+	PrimarySpecialty                      string
+	SecondarySpecialty1                   string
+	SecondarySpecialty2                   string
+	SecondarySpecialty3                   string
+	SecondarySpecialty4                   string
+	AllSecondarySpecialties               string
+	OrganizationLegalName                 string
+	GroupPracticePACID                    string
+	NumberOfGroupPracticeMembers          string
+	Line1StreetAddress                    string
+	Line2StreetAddress                    string
+	MarkerOfAddressLine2Suppression       string
+	City                                  string
+	State                                 string
+	ZipCode                               string
+	PhoneNumber                           string
+	HospitalAffiliationCCN1               string
+	HospitalAffiliationLBN1               string
+	HospitalAffiliationCCN2               string
+	HospitalAffiliationLBN2               string
+	HospitalAffiliationCCN3               string
+	HospitalAffiliationLBN3               string
+	HospitalAffiliationCCN4               string
+	HospitalAffiliationLBN4               string
+	HospitalAffiliationCCN5               string
+	HospitalAffiliationLBN5               string
+	ProfessionalAcceptsMedicareAssignment string
+	ReportedQualityMeasures               string
+	UsedElectronicHealthRecords           string
+	ParticipatedInTheMedicareMaintenance  string
+	CommittedToHeartHealth                string
+	specialtyID                           string
 }
 
 type FieldMismatch struct {
